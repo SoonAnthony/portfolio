e10b804aa19f4e816b36d7fc45d725154622165e
Anthony Soon <anthonymedia062@gmail.com>
2025-07-22 15:53:49 +0300

    Switch database to Supabase

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -99,8 +99,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': dj_database_url.config(
+        default='postgresql://postgres:41324088%23shisia@db.axabyinojfnoifnvphky.supabase.co:5432/postgres',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Auto Field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
